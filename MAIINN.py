9dcb398c83b358f9933cf9fb049aea530b0bee2a
Ediz Danacı <edizdanaci@gmail.com>
2026-07-26 14:13:57 +0300

    il pycharm commiti

---
+++ MAIINN.py
@@ -0,0 +1,11 @@
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+
+
+
+
+
+
+
---
+++ MAINN.py
@@ -0,0 +1,21 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+documents = [
+    "Cat cat CAT",
+    "CAT",
+    "cat"
+]
+
+vectorizer = CountVectorizer()
+
+X = vectorizer.fit_transform(documents)
+
+
+print(vectorizer.get_feature_names_out())
+print (X.toarray())
+
+
+
+
+
+
